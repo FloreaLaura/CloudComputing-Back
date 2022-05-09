7b4b3c9ef02ed715302683cfcd62d57bfae4e4ef
FloreaLaura <florealaura18@stud.ase.ro>
2022-05-09 14:05:02 +0300

    First routes and a router

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
 //index.js
-const express = require('express')
-const cors = require('cors')
+// const express = require('express')
+// const cors = require('cors')
+// const connection = require("./db.js");
+
+
+// const app = express();
+// app.use(cors())
+
+// const port = process.env.PORT || 8080;
+
+// // app.get('/', (req, res) => {
+// //   res.send('Hello World again!')
+// // });
+
+// app.listen(port, () => {
+//   console.log(`Cloud computing app listening on port ${port}!`)
+// });
+
+// // index.js
+// const express = require("express");
+// const connection = require("./db.js");
+
+// const app = express();
+// const port = process.env.PORT || 8080;
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}!`);
+// });
+
+// app.get("/messages", (req, res) => {
+//   connection.query("SELECT * FROM messages", (err, results) => {
+//     if (err) {
+//       return res.send(err);
+//     }
+
+//     return res.json({
+//       data: results,
+//     });
+//   });
+// });
+
+
+// index.js
+const express = require("express");
+const messagesRouter = require("./router/messagesRouter"); //look here
+const bodyParser = require("body-parser");
+
 const app = express();
-app.use(cors())
+// for parsing application/json
+app.use(bodyParser.json()); 
 
-const port = process.env.PORT || 8080;
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World again!')
-});
+app.use('/messages', messagesRouter); //and look here too
+
+const port = 8080;
 
 app.listen(port, () => {
-  console.log(`Cloud computing app listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}!`);
 });
