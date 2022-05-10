b8bb612701863b7d8351270f9b73732e5e1c83c2
FloreaLaura <florealaura18@stud.ase.ro>
2022-05-10 14:20:40 +0300

    Send mail

--- index.js
+++ index.js
@@ -1,62 +1,23 @@
-//index.js
-// const express = require('express')
-// const cors = require('cors')
-// const connection = require("./db.js");
-
-
-// const app = express();
-// app.use(cors())
-
-// const port = process.env.PORT || 8080;
-
-// // app.get('/', (req, res) => {
-// //   res.send('Hello World again!')
-// // });
-
-// app.listen(port, () => {
-//   console.log(`Cloud computing app listening on port ${port}!`)
-// });
-
-// // index.js
-// const express = require("express");
-// const connection = require("./db.js");
-
-// const app = express();
-// const port = process.env.PORT || 8080;
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}!`);
-// });
-
-// app.get("/messages", (req, res) => {
-//   connection.query("SELECT * FROM messages", (err, results) => {
-//     if (err) {
-//       return res.send(err);
-//     }
-
-//     return res.json({
-//       data: results,
-//     });
-//   });
-// });
-
-
-// index.js
-const express = require("express");
-const messagesRouter = require("./router/messagesRouter"); //look here
+const express = require('express');
+const cors = require('cors');
 const bodyParser = require("body-parser");
+const messagesRouter = require('./router/messagesRouter');
+const utilsRouter = require('./router/utilsRouter');
 
 const app = express();
+app.use(cors());
+
 // for parsing application/json
 app.use(bodyParser.json()); 
-
 // for parsing application/xwww-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/messages', messagesRouter); //and look here too
+// Define routes
+app.use("/messages", messagesRouter);
+app.use("/utils", utilsRouter);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+    console.log(`Cloud computing app listening on port ${port}!`)
 });
